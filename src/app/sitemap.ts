1adc54ab892c858f7a570d506324ebb6d70ea082
Mostafa Alasaad <info@alasaad.de>
2026-08-05 17:25:42 +0200

    Cloudflare static export

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,10 +1,12 @@
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://alasaad.de",
-      lastModified: new Date(),
+      lastModified: new Date("2026-08-05"),
       changeFrequency: "monthly",
       priority: 1,
     },
